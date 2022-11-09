0ea8374c66299ae2c6de27fde49c48388c67390e
Hiba <eng.lilil92@gmail.com>
2022-11-09 23:25:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', updateDom)
+
+function updateDom()
+{
+    const paragraph = document.getElementById('text');
+    paragraph.innerHTML = "This is really cool!"
+};
